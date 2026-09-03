ca57e8ab8c496550a82f6c94ca57dfb5e555bc18
KaioPzz <kaio.luiz.k20@gmail.com>
2026-09-02 22:00:57 -0300

    Correcoes ex4

--- ex4/Main.java
+++ ex4/Main.java
@@ -1,11 +1,15 @@
 package ex4;
 public class Main {
     public static void main(String[] args){
-        for(int i = 0; i < 5; i++);{
-            System.out.println(i);
-    }
-    for (int i = 100; i > 0; i++){
+    for (int i = 100; i > 0; i--) {
         System.out.println(i);
+    }
+    for (int x = 0; x < 10; x++){
+        System.out.println("#");
+        for (int y = 0; y < 2; y++){
+            System.out.print("#");
+        }
+        System.out.println("");
     }
  }
 }
